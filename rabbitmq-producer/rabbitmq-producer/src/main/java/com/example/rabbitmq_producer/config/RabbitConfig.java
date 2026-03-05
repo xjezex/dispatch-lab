be0505899a5def285d8b66ffa0240955117d3a13
Ezequiel Casada <ezequiel.casada2@globallogic.com>
2026-03-05 17:38:35 -0300

    feat(producer): add RabbitMQ configuration and bean definitions

---
+++ rabbitmq-producer/rabbitmq-producer/src/main/java/com/example/rabbitmq_producer/config/RabbitConfig.java
@@ -0,0 +1,31 @@
+package com.example.rabbitmq_producer.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String QUEUE = "my_queue";
+    public static final String EXCHANGE = "amq.direct";
+    public static final String ROUTING_KEY = "my_routing_key";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE, true); // durable queue
+    }
+
+    @Bean
+    public DirectExchange exchange() {
+        return new DirectExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, DirectExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+    }
+}
