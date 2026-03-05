3aa4b05be88e35ef7d468d40dd7544b0f7e8e794
Ezequiel Casada <ezequiel.casada2@globallogic.com>
2026-03-05 17:51:21 -0300

    fix: rename class

--- rabbitmq-producer/rabbitmq-producer/src/main/java/com/example/rabbitmq_producer/service/MessagePublisher.java
+++ rabbitmq-producer/rabbitmq-producer/src/main/java/com/example/rabbitmq_producer/service/MessagePublisher.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 @Slf4j
 @RequiredArgsConstructor
-public class MessagePublishe {
+public class MessagePublisher {
 
     private final RabbitTemplate rabbitTemplate;
 
